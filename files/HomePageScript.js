63da7b71ece7bfe2fc00865375be5144e138acaf
coreycaskey <ccaskey6@gatech.edu>
2017-12-06 15:57:47 -0500

    Push Over SportPort Files

---
+++ files/HomePageScript.js
@@ -0,0 +1,10 @@
+// function showAlert(name, email, message) {
+//   alert(name + " : " + email + " : " + message);
+//   document.getElementById('userName').value = "";
+//   document.getElementById('email').value = "";
+//   document.getElementById('textMessage').value = "";
+// }
+
+function showAlert() {
+  alert("Message sent!");
+}
